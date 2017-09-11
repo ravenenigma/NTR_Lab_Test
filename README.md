f7a033681a18d75cf9fe86f117a66be02310fbb1
EnigmaRaven <ravshanenigma@gmail.com>
2017-09-11 16:17:07 +0700

    Test

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NTR_Lab_Test
+
+1.  Write a listbox-style binary search for an ordered array of integers.  Listbox-style means that you should return the index of the first item greater than or equal to the item being searched for; if all items are less, you should return the index of the last item.  You are guaranteed that there is at least one item in the array.
+
+Перевод: Написать listbox-style бинарный поиск для упорядоченного массива целых чисел. Listbox-style значит что вы должны вернуть индекс первого элемента, который больше или равен значению, которое ищем. Если все элеменыт меньше, то вернуть индекс последнего элемента. Вам гарантируют что массив заполнен хотя бы одним значением. 
+
+
+2.  Suppose you have an array of integers, both positive and negative, in no particular order.  Find the largest possible sum of any continuous subarray.  
+For example, if you have all positive numbers, the largest sum would be the sum of the whole array; if you have all negative numbers, the largest sum is 0 (the null subarray)
+
+Перевод: Предположим, что у вас есть массив целых чисел, как положительных, так и отрицательных, в определенном порядке. Найдите максимально возможную сумму любого непрерывного подмассива. Для примера, если вы имеете все положительные числа наибольшая сумма будет сумма всего массива; если вы имеете все отрицательные числа наибольшая сумма ноль (нулевой подмассив).
---
+++ Test/Test/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 3, 5, 7, 9, 11};
+            int[] arr = {5, -9, 6, -2, 3};
+            int[] maxArray = {4, 5, 2, 1, 7, 9};
+            int[] minArray = {-6, -2, -3, -8, -1, -4};
+
+            Console.WriteLine("\tДвоичный (бинарный) поиск!!!\n");
+
+            Console.WriteLine("Ищем -1: {0}", BinarySearch(array, -1));
+            Console.WriteLine("Ищем  3: {0}", BinarySearch(array, 3));
+            Console.WriteLine("Ищем  6: {0}", BinarySearch(array, 6));
+            Console.WriteLine("Ищем  9: {0}", BinarySearch(array, 9));
+            Console.WriteLine("Ищем 11: {0}", BinarySearch(array, 11));
+            Console.WriteLine("Ищем 20: {0}", BinarySearch(array, 20));
+
+            Console.WriteLine("\n\tПоиск максимальной непрерывной подпоследовательности!!!\n");
+
+            Console.WriteLine("Максимальное значение массива: {0}", GetMaxSum(arr));
+            Console.WriteLine("Максимальное значение отрицательных элементов массива: {0}", GetMaxSum(minArray));
+            Console.WriteLine("Максимальное значение положительныъ элементов массива: {0}", GetMaxSum(maxArray));
+
+            Console.WriteLine("\nНажмите любую клавишу для завершения программы....");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Бинарный поиск в массиве.
+        /// </summary>
+        /// <param name="array"> Массив типа int[] </param>
+        /// <param name="value"> Искомый элемент. </param>
+        /// <returns></returns>
+        public static int BinarySearch(int[] array, int value)
+        {
+            // Проверить, имеет ли смыл вообще выполнять поиск:
+            // - если длина массива равна нулю - искать нечего.
+            if (array.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException("array", "Массив должен содержать, как минимум, один элемент.");
+            }
+            
+            // Номер первого элемента в массиве.
+            var firstIndex = 0;
+
+            // Номер элемента массива, СЛЕДУЮЩЕГО за последним
+            var lastIndex = array.Length - 1;
+
+            // Eсли искомый элемент меньше первого элемента массива, значит, вернем первый элемент.
+            if (array[firstIndex] >= value)
+                return firstIndex;
+
+            // Eсли искомый элемент больше последнего элемента массива, значит, вернем последний элемент.
+            if (array[lastIndex] <= value)
+                return lastIndex;
+
+            // Приступить к поиску.
+            // Если просматриваемый участок не пуст, first <= last
+            while (firstIndex <= lastIndex)
+            {
+                var middle = (firstIndex + lastIndex) / 2;
+
+                if (value < array[middle])
+                {
+                    lastIndex = middle - 1;
+                }
+                else if (value > array[middle])
+                {
+                    firstIndex = middle + 1;
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+            return firstIndex;
+        }
+
+        /// <summary>
+        /// Поиск максимальной непрерывной подпоследовательности.
+        /// </summary>
+        /// <param name="array"> Массив типа int[] </param>
+        /// <returns></returns>
+        public static int GetMaxSum(int[] array)
+        {
+            int sum = 0;
+            int maxSum = 0;
+
+            foreach (var item in array)
+            {
+                if (int.MaxValue - sum < item)
+                    throw new Exception("Сумма элементов подмассива слишком велика.");
+
+                sum += item;
+
+                if (sum < 0)
+                {
+                    sum = 0;
+                }
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
